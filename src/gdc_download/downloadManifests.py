825f7b9873d065ef4d2be3faeb6c54ce100e9860
eye942 <eye942@gmail.com>
2016-12-22 17:13:32 -0700

    Update downloadManifests.py

--- src/gdc_download/downloadManifests.py
+++ src/gdc_download/downloadManifests.py
@@ -14,12 +14,11 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import urllib
-import shutil2
 import subprocess
 import os
 
-
-
+dest = '\\\\sii-nas3/Data/NCI-GDC'
+gdc_path = ''
 
 binary = FirefoxBinary('C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe')
 
@@ -34,34 +33,64 @@ with open('cancers.txt','r') as f:
         if splLine != '': 
             cancers.append()
 
-queries = ['(cases.project.project_id in [#CANCER] and files.access in ["open"])',
-           '(files.data_category in ["Simple Nucleotide Variation"] and files.analysis.workflow_type in ["MuTect2 Variant Aggregation and Masking"])',
-           '(files.data_category in ["Copy Number Variation"] and files.data_type in ["Copy Number Segment"])',
-           '(files.data_type in ["Gene Expression Quantification"] and files.analysis.workflow_type in ["HTSeq - FPKM"] and files.access in ["open"])',
-           '(files.data_type in ["miRNA Expression Quantification"] and files.experimental_strategy in ["miRNA-Seq"])',
-           '(files.data_category in ["DNA Methylation"])'
-           ]             
+queryS = ['(cases.project.project_id in [','] and files.access in ["open"])']
+queries = ['(files.data_category in ["Simple Nucleotide Variation"] and files.analysis.workflow_type in ["MuTect2 Variant Aggregation and Masking"])',
+         '(files.data_category in ["Copy Number Variation"] and files.data_type in ["Copy Number Segment"])',
+         '(files.data_type in ["Gene Expression Quantification"] and files.analysis.workflow_type in ["HTSeq - FPKM"] and files.access in ["open"])',
+         '(files.data_type in ["miRNA Expression Quantification"] and files.experimental_strategy in ["miRNA-Seq"])',
+         '(files.data_category in ["DNA Methylation"])'
+        ]             
             
 for cancer in cancers:
+    #Create a folder in the destination to store the results
+    os.mkdir(dest+'/'+cancer)
+    #Base of the search string with the project name integrated
+    qString = queryS[0]+ cancer +queryS[1]
+    
+    #Go to website
     driver.get(base_url + '/query/s?query=' + 
                urllib.quote(
-                                queries[0] + ' and (' + queries[1] + ' or ' + 
-                                queries[2]+' or '+queries[3]+' or ' + 
-                                queries[4] + ' or ' + queries[5] +')'
+                                qString + ' and (' + queries[0] + ' or ' + 
+                                queries[1]+' or '+queries[2]+' or ' + 
+                                queries[3] + ' or ' + queries[4] +')'
                                 )
                 )
+    #Click on the banner that says that the site is a gov't website
+    driver.find_element_by_css_selector("button.btn.btn-primary").click()
+    #wait 10s for page to load
+    driver.implicitly_wait(10)
+    #Click search query
+    driver.find_element_by_xpath("//div[@id='skip']/div/div/div/search-bar/div/div/div/div[2]/button").click()
+    #wait 10s for page to load
+    driver.implicitly_wait(10)
+    #Click on download manifest
     driver.find_element_by_css_selector("span.ng-binding.ng-scope").click()
-    for ftype in queries[1:]:
-        driver.get(base_url + '/query/s?query=' + urllib.quote(queries[0] + ' and (' + ftype +')'))
+    
+    for ftype in queries:
+        #Go to website
+        driver.get(base_url + '/query/s?query=' + urllib.quote(qString + ' and (' + ftype +')'))
+        #wait 10s for page to load
+        driver.implicitly_wait(10)
+        #Click search query
+        driver.find_element_by_xpath("//div[@id='skip']/div/div/div/search-bar/div/div/div/div[2]/button").click()
+        #wait 10s for page to load
+        driver.implicitly_wait(10)
+        #Click on add to cart
         driver.find_element_by_id("add-to-cart-button").click()
+        #Click view cart
         driver.find_element_by_css_selector("i.fa.fa-shopping-cart").click()
+        #Click on Download Metadata
         driver.find_element_by_xpath("//div[@id='skip']/div/section[2]/div/div/button").click()
+        #Click clear cart
         driver.find_element_by_css_selector("#split-control-1482191843912 > span > span.ng-binding.ng-scope").click()
+        #Wait 1s for button to appear
+        driver.implicitly_wait(1)
+        #Click clear all items in cart
         driver.find_element_by_id("clear-button").click()
         
     file_names = [filenames for (a, b, filenames) in os.walk(downloadDir)]
     
-    subprocess.call()   
+    subprocess.call([gdc_path+'/gdc-client', 'download','-d', dest+'/'+cancer,'--no-segment-md5sums','--no-related-files','--no-annotations', '-m', manifest])   
 
-
+    
 
