ca5777054b91fccea86282b188a1c473d6fc6f30
eye942 <eye942@gmail.com>
2017-01-11 17:27:16 -0700

    Update downloadManifests.py

--- src/gdc_download/downloadManifests.py
+++ src/gdc_download/downloadManifests.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec 19 16:23:18 2016
-
 @author: Eric Ye
 """
 
@@ -17,14 +16,15 @@ import urllib
 import subprocess
 import os
 import re
-import time
+import threading
+import queue
 from datetime import datetime
-import shutil2
+import shutil
 import collections
-
-    
-def script(downloadDir,dest='\\\\sii-nas3/Data/NCI-GDC', gdc_path='', 
-           mozillaPath = 'C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe',
+import sys,getopt
+ 
+def download_cancer(q,cancer,downloadDir,dest='\\\\sii-nas3/Data/NCI-GDC', gdc_path='', 
+           mozillaPath = 'C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe'
            ):
     
     
@@ -34,13 +34,9 @@ def script(downloadDir,dest='\\\\sii-nas3/Data/NCI-GDC', gdc_path='',
     driver = webdriver.Firefox(firefox_binary=binary)
     driver.implicitly_wait(30)
     base_url = "https://gdc-portal.nci.nih.gov"
+    cancer_search = re.match('+-(.+)',cancer)
+    cancer_name = cancer_search.group(1)
     
-    with open('cancers.txt','r') as f:
-        cancers = []
-        for line in f:
-            splLine = line.split(' ')[0]
-            if splLine != '': 
-                cancers.append()
     
     queryS = ['(cases.project.project_id in [','] and files.access in ["open"])']
     queries = ['(files.data_category in ["Simple Nucleotide Variation"] and files.analysis.workflow_type in ["MuTect2 Variant Aggregation and Masking"])',
@@ -50,81 +46,86 @@ def script(downloadDir,dest='\\\\sii-nas3/Data/NCI-GDC', gdc_path='',
              '(files.data_category in ["DNA Methylation"])'
             ]             
     qName = ['snv','cnv','gene_expr','mirna','meth']
-    for cancer in cancers:
-        #Create a folder in the destination to store the results
-        os.mkdir(dest+'/'+cancer)
-        #Base of the search string with the project name integrated
-        qString = queryS[0]+ cancer +queryS[1]
         
+    
+    #Create a folder in the destination to store the results
+    os.mkdir(dest+'/'+cancer_name)
+    #Base of the search string with the project name integrated
+    qString = queryS[0]+ cancer +queryS[1]
+    
+    #Go to website
+    driver.get(base_url + '/query/s?query=' + 
+               urllib.quote(
+                                qString + ' and (' + queries[0] + ' or ' + 
+                                queries[1]+' or '+queries[2]+' or ' + 
+                                queries[3] + ' or ' + queries[4] +')'
+                                )
+                )
+    #Click on the banner that says that the site is a gov't website
+    driver.find_element_by_css_selector("button.btn.btn-primary").click()
+    #wait 10s for page to load
+    driver.implicitly_wait(10)
+    #Click search query
+    driver.find_element_by_xpath("//div[@id='skip']/div/div/div/search-bar/div/div/div/div[2]/button").click()
+    #wait 10s for page to load
+    driver.implicitly_wait(10)
+    #Click on download manifest
+    driver.find_element_by_css_selector("span.ng-binding.ng-scope").click()
+    for ftype in queries:
         #Go to website
-        driver.get(base_url + '/query/s?query=' + 
-                   urllib.quote(
-                                    qString + ' and (' + queries[0] + ' or ' + 
-                                    queries[1]+' or '+queries[2]+' or ' + 
-                                    queries[3] + ' or ' + queries[4] +')'
-                                    )
-                    )
-        #Click on the banner that says that the site is a gov't website
-        driver.find_element_by_css_selector("button.btn.btn-primary").click()
+        driver.get(base_url + '/query/s?query=' + urllib.quote(qString + ' and (' + ftype +')'))
         #wait 10s for page to load
         driver.implicitly_wait(10)
         #Click search query
         driver.find_element_by_xpath("//div[@id='skip']/div/div/div/search-bar/div/div/div/div[2]/button").click()
         #wait 10s for page to load
         driver.implicitly_wait(10)
-        #Click on download manifest
-        driver.find_element_by_css_selector("span.ng-binding.ng-scope").click()
-        for ftype in queries:
-            #Go to website
-            driver.get(base_url + '/query/s?query=' + urllib.quote(qString + ' and (' + ftype +')'))
-            #wait 10s for page to load
-            driver.implicitly_wait(10)
-            #Click search query
-            driver.find_element_by_xpath("//div[@id='skip']/div/div/div/search-bar/div/div/div/div[2]/button").click()
-            #wait 10s for page to load
-            driver.implicitly_wait(10)
-            #Click on add all to cart
-            driver.find_element_by_id("add-to-cart-button").click()
-            #Click view cart
-            driver.find_element_by_css_selector("i.fa.fa-shopping-cart").click()
-            #wait 60s for page to load
-            driver.implicitly_wait(60)
-            #Click on Download Metadata
-            driver.find_element_by_xpath("//div[@id='skip']/div/section[2]/div/div/button").click()
-            #Click clear cart
-            driver.find_element_by_css_selector("#split-control-1482191843912 > span > span.ng-binding.ng-scope").click()
-            #Wait 1s for button to appear
-            driver.implicitly_wait(1)
-            #Click clear all items in cart
-            driver.find_element_by_id("clear-button").click()
-        
-        manifest = getManifest(downloadDir)
-        subprocess.call([gdc_path+'/gdc-client', 'download','-d', dest+'/'+cancer,'--no-segment-md5sums','--no-related-files','--no-annotations', '-m', manifest])   
-        
-        fileSort = comp_time(downloadDir)
-        for meta,prefix in zip(fileSort,qName):
-            shutil2.move(downloadDir+'/'+meta, dest+'/'+ prefix+'_'+cancer+'.json')
+        #Click on add all to cart
+        driver.find_element_by_id("add-to-cart-button").click()
+        #Click view cart
+        driver.find_element_by_css_selector("i.fa.fa-shopping-cart").click()
+        #wait 60s for page to load
+        driver.implicitly_wait(60)
+        #Click on Download Metadata
+        driver.find_element_by_xpath("//div[@id='skip']/div/section[2]/div/div/button").click()
+        #Click clear cart
+        driver.find_element_by_css_selector("#split-control-1482191843912 > span > span.ng-binding.ng-scope").click()
+        #Wait 1s for button to appear
+        driver.implicitly_wait(1)
+        #Click clear all items in cart
+        driver.find_element_by_id("clear-button").click()
+    # get the manifest file 
+    manifest = getManifest(downloadDir) 
+    # Run the gdc-client with the manifest
+    subprocess.call([gdc_path+'/gdc-client', 'download','-d', dest+'/'+cancer_name,'--no-segment-md5sums','--no-related-files','--no-annotations', '-m', manifest])   
+    os.remove(manifest)
+    fileSort = comp_time(downloadDir)
+    for meta,prefix in zip(fileSort,qName):
+        shutil.move(downloadDir+'/'+meta, dest+'/'+ prefix+'_'+cancer_name+'.json')
+    q.task_done()
     
 
     
 def comp_time(downDir):
-    flist = os.listdir(downDir)
-    pattern = re.compile("(?P<Year>[0-9]{4})\-(?P<Month>[0-9]{2})\-(?P<Date>[0-9]{2})T(?P<Hour>[0-9]{2})\_(?P<Minute>[0-9]{2})\_(?P<Second>[0-9]{2})\.(?P<Precision>[0-9]{6})\.json")
-    tdic = {}
-    for i in flist:
-        pattmatch = pattern.match(i)
-        if pattmatch != None:
-            a = datetime(pattmatch.group('Year'),pattmatch.group('Month'),
-                         pattmatch.group('Date'),pattmatch.group('Hour'),
-                         pattmatch.group('Minute'),pattmatch.group('Second'),
-                         pattmatch.group('Precision')
-                         )
-            tdic[a] = i
-    fdic = collections.OrderedDict(sorted(tdic.items()))
-    sortVals = fdic.values()
-    assert (len(sortVals) <=5)
-    if len(sortVals)!= 5:
-        comp_time(downDir)
+    while True:
+        flist = os.listdir(downDir)
+        pattern = re.compile("(?P<Year>[0-9]{4})\-(?P<Month>[0-9]{2})\-(?P<Date>[0-9]{2})T(?P<Hour>[0-9]{2})\_(?P<Minute>[0-9]{2})\_(?P<Second>[0-9]{2})\.(?P<Precision>[0-9]{6})\.json")
+        tdic = {}
+        for i in flist:
+            pattmatch = pattern.match(i)
+            if pattmatch != None:
+                a = datetime(pattmatch.group('Year'),pattmatch.group('Month'),
+                             pattmatch.group('Date'),pattmatch.group('Hour'),
+                             pattmatch.group('Minute'),pattmatch.group('Second'),
+                             pattmatch.group('Precision')
+                             )
+                tdic[a] = i
+        fdic = collections.OrderedDict(sorted(tdic.items()))
+        sortVals = fdic.values()
+        assert (len(sortVals) <=5)
+        if len(sortVals) == 5:
+            return sortVals
+            
         
 
 def getManifest(downDir):
@@ -133,7 +134,61 @@ def getManifest(downDir):
         check = False
         for i in flist:
             if i[-4:] == '.tsv':
+                os.remove(i)
                 return i
             if i[:12] == 'gdc_manifest':
                 check = True
         assert (check)
+
+
+def multithread(cancers_dir = 'C:\\Users\\localadmin\\Downloads\\cancers.txt',
+                download_dir = 'C:\\Users\\localadmin\\Downloads',
+                dest='\\\\sii-nas3/Data/NCI-GDC', gdc_path='C:\\Users\\localadmin\\Downloads', 
+                mozillaPath = 'C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe'):
+    with open(cancers_dir,'r') as f:
+            cancers = []
+            for line in f:
+                cancers.append(line)
+    thread_q = queue.Queue(4)                
+    for cancer in cancers:
+        t = threading.Thread(target=download_cancer, args=(cancer,download_dir))
+        t.start()
+        thread_q.put(t)
+
+def main():        
+    cancer_dir = 'C:\\Users\\localadmin\\Downloads\\cancers.txt'
+    download_dir = 'C:\\Users\\localadmin\\Downloads'
+    dest='\\\\sii-nas3/Data/NCI-GDC'
+    gdc_path='C:\\Users\\localadmin\\Downloads'
+    mozillaPath = 'C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe'
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"hg",["help","c_dir=","dest=","g_path=","m_path="])
+    except getopt.GetoptError:
+        print( 'downloadManifests.py -g --c_dir <directory of cancer list> '
+              +'--dest <directory to save the files>'+
+              ' --g_path <path of the gdc-client> --m_path <path of lastest mozilla browser>')
+        sys.exit(2)
+    for o,a in opts:
+        if o in ("-h","--help"):
+            print( 'downloadManifests.py -g --c_dir <directory of cancer list> '
+              +'--dest <directory to save the files>'+
+              ' --g_path <path of the gdc-client> --m_path <path of lastest mozilla browser>')
+        elif o == "--c_dir":
+            cancer_dir = a
+        elif o == "--dest":
+            dest = a
+        elif o == "--g_path":
+            gdc_path = a
+        elif o == "--m_path":
+            mozillaPath = a
+        else:
+            assert False, "unhandled option"
+    if len(args) ==1 :
+        download_dir = args[1]
+    elif len(args) > 1:
+        assert False, "too many arguments"
+    multithread(cancer_dir, download_dir,dest, gdc_path, mozillaPath)
+
+
+if __name__== "__main__":
+    main(sys.argv[1:])
