eea9edfc1b4ed6322c2e8b0b73d7bf0166f36595
Eric Y <eye942@gmail.com>
2017-01-23 17:00:47 -0700

    Syntax beautification and write_files complete

--- src/gdc_download/APIdownload.py
+++ src/gdc_download/APIdownload.py
@@ -3,14 +3,15 @@ import json
 import re
 import requests
 import subprocess
+import os
 
 #import json requests
 with open('queries.json') as f:
-    json_queries = json.load(f)
+    JSON_QUERIES = json.load(f)
 
 #import cancer names to be used in json requests
 with open('cancers.txt') as f:
-    cancers = [cancer for cancer in f]
+    CANCERS = [cancer for cancer in f]
 
 #import the metadata fields normally seen
 with requests.get('https://gdc-api.nci.nih.gov/files/_mapping') as f:
@@ -19,12 +20,12 @@ with requests.get('https://gdc-api.nci.nih.gov/files/_mapping') as f:
     expand_string = expand_fields.join(',')
     expand_req_string = '?expand=' + expand_string
 
-def download_manifest(cancer, path):
+def download_manifest(cancer, path, dest):
     """ Downloads the manifest from NCI-GDC via API using
         the cancer (i.e. project name) and path for a set of queries.
     """
-    manifest_query = json_queries.copy()
-    manifest_query['main_request']['content'].extend(json_queries['requests'])
+    manifest_query = JSON_QUERIES.copy()
+    manifest_query['main_request']['content'].extend(JSON_QUERIES['requests'])
 
     #Replace the cancer filter placeholder with the cancer name
     cancer_dict = manifest_query['main_request']['content'][0]
@@ -37,16 +38,16 @@ def download_manifest(cancer, path):
 
     #Execute the http get url
     name = re.match('.+-(.+)', cancer)
-    file_name = path+'/'+name.group(1)+'.tsv'
+    file_name = path + '/' + name.group(1) + '.tsv'
     api_response = requests.get(query_url)
 
     #Write manifest to file
-    with open(file_name,'w') as f:
+    with open(file_name, 'w') as f:
         for line in api_response.iter_lines():
             f.write(line+'\n')
     return f.exists()
 
-def download_other_manifests(cancer,path):
+def download_other_manifests(cancer, path):
     """ DocString goes here
     """
     #initiate manifest list
@@ -56,14 +57,14 @@ def download_other_manifests(cancer,path):
     name = re.match('.+-(.+)', cancer).group(1)
 
     #Use main query
-    manifest_query = json_queries.copy()
+    manifest_query = JSON_QUERIES.copy()
     cancer_dict = manifest_query['main_request']['content'][0]
     cancer_dict['value'] = [cancer]
     manifest_query['main_request']['content'][0] = cancer_dict
     manifest_query = manifest_query['main_request']
 
     #Instantiate requests for each type of manifest
-    for i in json_queries["requests"]:
+    for i in JSON_QUERIES["requests"]:
         temp_query = manifest_query.copy()
         temp_dic = temp_query['content']
         temp_dic.append(i)
@@ -79,26 +80,27 @@ def download_other_manifests(cancer,path):
         prefixes = [prefix for prefix in f]
 
     #Ask for requests in sequence
-    manifests = ['https://gdc-api.nci.nih.gov/files?filters='+query+'&size=30000&return_type=manifest' for query in manifests]
+    manifests = ['https://gdc-api.nci.nih.gov/files?filters='+query+'&size=30000&return_type=manifest' 
+                 for query in manifests]
 
-    for prefix,query in zip(prefixes, manifests):
-        
-        file_name = prefix + '_' + name + '.tsv'
-        
+    for prefix, query in zip(prefixes, manifests):
+        file_name = path + prefix + '_' + name + '.tsv'
+
         response = requests.get(query)
         json_response = response.json()
-        
+
         #Write the response to a file
         with open(file_name, 'w') as f:
-            json.dump(json_response,f)
+            json.dump(json_response, f)
 
-def write_metadata(manifest_path, dest):
-    
+def write_metadata(manifest_path, path):
+    """ DocString Goes Here
+    """
     #Get the file uuids from manifest
     with open(manifest_path) as f:
         file_ids = [i[0:i.find('\t')] for i in f]
         file_ids.pop()
-    
+
     #Metadata list
     meta_list = []
 
@@ -109,14 +111,18 @@ def write_metadata(manifest_path, dest):
         meta_list.append(json_response)
 
     #Make pretty json file with metadata    
-    with open(manifest_path[:-4] + '.json') as f:
+    with open(path + manifest_path[:-4] + '.json') as f:
         json.dump(meta_list, f)
 
+    os.remove(manifest_path)
 
-def write_files(manifest, dest):
+
+def write_files(manifest_path, gdc_path, path):
     
     #Download Files
-    subprocess.call([gdc_path+'/gdc-client', 'download','-d', dest+'/'+cancer_name,
-            '--no-segment-md5sums','--no-related-files','--no-annotations', '-m', manifest_path])
+    subprocess.call([gdc_path+'/gdc-client', 'download', '-d', path,
+                     '--no-segment-md5sums', '--no-related-files', '--no-annotations',
+                     '-m', manifest_path])
 
+    os.remove(manifest_path)
     
