83cdf5f7cda78aed78e3c5fae155e919579d2b16
eye942 <eye942@gmail.com>
2017-01-24 07:29:45 -0700

    not yet complete

--- src/gdc_download/APIdownload.py
+++ src/gdc_download/APIdownload.py
@@ -15,34 +15,38 @@ with open('cancers.txt') as f:
 with requests.get('https://gdc-api.nci.nih.gov/files/_mapping') as f:
     json_field_map = json.load(f)
     expand_fields = json_field_map["expand"]
-    expand_string = expand_fields.join(',')
+    expand_string = string.join(expand_fields, ',')
     expand_req_string = 'expand?=' + expand_string
 
-def download_manifest(cancer,path):
+def download_manifest(cancer, path):
+    """
+    """
     manifest_query = json_queries
     manifest_query['main_request']['content'].extend(json_queries['requests'])
-    
+
     #Replace the cancer filter placeholder with the cancer name
     cancer_dict = manifest_query['main_request']['content'][0]
     cancer_dict['value'] = [cancer]
     manifest_query['main_request']['content'][0] = cancer_dict
-    
+
     #Create the http get url
-    query = urllib.urlencode(json.dumps(a))
-    query_url = 'https://gdc-api.nci.nih.gov/files?filters='+query+'&size=30000&return_type=manifest'
-    
+    query = urllib.urlencode(json.dumps(manifest_query))
+    query_url = 'https://gdc-api.nci.nih.gov/files?filters=' + 
+                    query + '&size=30000&return_type=manifest'
+
     #Execute the http get url
-    name = re.match('.+-(.+)',cancer)
+    name = re.match('.+-(.+)', cancer)
     file_name = path+'/'+name.group(1)+'.tsv'
     api_response = requests.get(query_url)
-    
+
     #Write manifest to file
-    with open(file_name,'w') as f:
+    with open(file_name, 'w') as f:
         for line in api_response.iter_lines():
             f.write(line+'\n')
     return f.exists()
 
-
-def write_metadata(manifest_path):
+def write_metadata(manifest_path):    
     with open(manifest_path) as f:
         file_ids = [i[0:i.find('\t')] for i in f]
+        file_ids.pop()
+
